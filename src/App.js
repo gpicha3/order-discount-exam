40a0ec4badf94271cd21b894dd44a14b5bfc70bd
gpicha3 <gamepichaya2@gmail.com>
2024-02-12 17:41:59 +0700

    LOGIC CALCULATE DISCOUNT

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
 
   const [totalPrice , setTotalPrice] = useState(0)
   const [userPoint , setUserPoint] = useState(100)
+  const [backupUserPoint , setBackupUserPoint] = useState(userPoint)
 
   const [coupons , setCoupons] = useState([])
   const [ontops , setOntops] = useState([])
@@ -18,14 +19,23 @@ function App() {
 
   const [couponSelect , setCouponSelect] = useState(0)
   const [ontopSelect , setOnTopSelect] = useState(0)
-  const [seasonalsSelect , setSeasonalSelect] = useState(0)
+  const [seasonalSelect , setSeasonalSelect] = useState(0)
 
-  useEffect(() => {
+  const [couponDiscount , setCouponDiscount] = useState(0)
+  const [ontopDiscount , setOnTopDiscount] = useState(0)
+  const [seasonalDiscount , setSeasonalDiscount] = useState(0)
+
+  const findTotolPrice = () =>{
     let sum = 0;
     cart.forEach((item) => {
       sum += item.price;
     });
-    setTotalPrice(sum);
+
+    return sum
+  }
+
+  useEffect(() => {
+    setTotalPrice(findTotolPrice());
 
     const coupon = []
     coupon.push(createCoupon(1 , "Fixed amount" , 50))
@@ -47,8 +57,68 @@ function App() {
   const selected = "coupon-item-selected"
 
   const calculateTotalPrice = () => {
+    let price = findTotolPrice()
 
+    if(couponSelect !== 0){
+      const coupon = coupons.find((item)=>{
+        return item.id === couponSelect
+      })
+      if(coupon.coupon === "Fixed amount"){
+        setCouponDiscount(coupon.discount)
+        price -= coupon.discount
+      }else if(coupon.coupon === "Percentage discount"){
+        let discount = (price)*coupon.discount/100
+        setCouponDiscount(discount)
+        price -= discount
+      }
+    }
+
+    if(ontopSelect !== 0){
+      const ontop = ontops.find((item) => {
+        return item.id === ontopSelect
+      })
+      if(ontop.categoryProduct !== "Customer points"){
+        let discount = 0;
+        const itemDiscount = cart.filter((item) => {
+          return item.category === ontop.categoryProduct
+        })
+        itemDiscount.map((item) => {
+          discount += item.price * ontop.discount/100
+        })
+        setOnTopDiscount(discount)
+        price -= discount
+      }else {
+        let maxDiscount = price*20/100
+        if(userPoint > maxDiscount){
+          price -= maxDiscount
+          setOnTopDiscount(maxDiscount)
+          setUserPoint(userPoint - maxDiscount)
+        }else{
+          price -= userPoint
+          setOnTopDiscount(userPoint)
+          setUserPoint(0)
+        }
+      }
+    }
+
+    if(seasonalSelect !== 0){
+      const seasonal = seasonals.find((item) => {
+        return item.id === seasonalSelect
+      })
+      let discount = Math.floor(price / seasonal.every) * seasonal.discount
+      price -= discount
+      setSeasonalDiscount(discount)
+    }
+
+    setTotalPrice(price)
   }
+
+  useEffect(() => {
+    calculateTotalPrice();
+    if(ontopSelect === 0){
+      setUserPoint(backupUserPoint);
+    }
+  } , [couponSelect , ontopSelect , seasonalSelect])
 
   return (
     <div className="App">
@@ -69,6 +139,12 @@ function App() {
               )
             })}
           </table>
+          <div className='discount-total'>
+            <h5 style={{display: `${couponSelect !== 0 || ontopSelect !== 0 || seasonalSelect !== 0? "block" : "none"}`}}>Discount</h5>
+            <h6 style={{display: `${couponSelect !== 0? "block" : "none"}`}}> - {couponDiscount} $</h6>
+            <h6 style={{display: `${ontopSelect !== 0? "block" : "none"}`}}> - {ontopDiscount} $</h6>
+            <h6 style={{display: `${seasonalSelect !== 0? "block" : "none"}`}}> - {seasonalDiscount} $</h6>
+          </div>
           <div className='container-total'>
             <h5>Total : ${totalPrice}</h5>
           </div>
@@ -84,7 +160,7 @@ function App() {
             {coupons.map((coupon) => {
               return (
                 <div className={`${couponSelect === coupon.id? selected : "coupon-item"}`} onClick={() => {couponSelect === coupon.id? setCouponSelect(0) : setCouponSelect(coupon.id)}}>
-                  <h6>{coupon.coupon} : {coupon.discount}</h6>
+                  <h6>{coupon.coupon} : {coupon.discount} {coupon.coupon === "Percentage discount"? "%" : "$"}</h6>
                 </div>
               )
             })}
@@ -96,7 +172,7 @@ function App() {
             {ontops.map((coupon) => {
               return (
                 <div className={`${ontopSelect === coupon.id? selected : "coupon-item"}`} onClick={() => {ontopSelect === coupon.id? setOnTopSelect(0) : setOnTopSelect(coupon.id)}}>
-                  <h6>{coupon.categoryProduct} : {coupon.discount}</h6>
+                  <h6>{coupon.categoryProduct} {coupon.categoryProduct !== "Customer points"? `discount : ${coupon.discount}%` : "for discount"}</h6>
                 </div>
               )
             })}
@@ -107,8 +183,8 @@ function App() {
             <div className='selection-coupon'>
             {seasonals.map((coupon) => {
               return (
-                <div className={`${seasonalsSelect === coupon.id? selected : "coupon-item"}`} onClick={() => {seasonalsSelect === coupon.id? setSeasonalSelect(0) : setSeasonalSelect(coupon.id)}}>
-                  <h6>Every {coupon.every} <br/>discount {coupon.discount}</h6>
+                <div className={`${seasonalSelect === coupon.id? selected : "coupon-item"}`} onClick={() => {seasonalSelect === coupon.id? setSeasonalSelect(0) : setSeasonalSelect(coupon.id)}}>
+                  <h6>Every {coupon.every} $ discount {coupon.discount} $</h6>
                 </div>
               )
             })}
